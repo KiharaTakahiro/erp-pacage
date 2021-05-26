054de3214c25ccb49c6a2be245bdfa28b638cf01
永戸健太郎 <kntru0218gj@gmail.com>
2021-05-26 16:59:06 +0900

    倉庫追加処理

---
+++ erp/src/main/java/com/erp/main/app/controller/system/request/CreateWarehouseRequest.java
@@ -0,0 +1,24 @@
+package com.erp.main.app.controller.system.request;
+
+import com.erp.main.domain.objects.valueObjects.CreateWarehouseVo;
+import com.sun.istack.NotNull;
+
+import lombok.Data;
+
+@Data
+public class CreateWarehouseRequest {
+	/*
+	 * 倉庫名
+	 */
+	@NotNull
+	private String warehouseName;
+	
+	public CreateWarehouseVo mapTo() {
+		 CreateWarehouseVo vo = new  CreateWarehouseVo();
+		// 商品名
+		vo.setWarehouseName(this.getWarehouseName());
+		return vo;
+	}
+	
+
+}
---
+++ erp/src/main/java/com/erp/main/domain/objects/valueObjects/CreateWarehouseVo.java
@@ -0,0 +1,17 @@
+package com.erp.main.domain.objects.valueObjects;
+
+import lombok.Data;
+
+/*
+ * 倉庫作成Vo
+ * @autho ngt
+ */
+
+@Data
+public class CreateWarehouseVo {
+	/*
+	 * 倉庫名
+	 */
+	private String warehouseName;
+
+}
