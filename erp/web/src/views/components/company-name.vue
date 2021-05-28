1f781359cfa5b2cdb3a37ab004c1abbfceab8b8d
永戸健太郎 <kntru0218gj@gmail.com>
2021-05-28 18:04:05 +0900

    一旦コミット

---
+++ erp/web/src/views/components/company-name.vue
@@ -0,0 +1,47 @@
+
+
+<template>
+  <el-form-item prop="name">
+  <el-input
+    ref="client"
+    v-model="client.name"
+    :placeholder="$t('client.name')"
+    name="name"
+    type="text"
+    tabindex="1"
+    autocomplete="on"
+    max="50"
+    style="width:100%; margin-bottom:30px;"
+  />
+</el-form-item>
+</template>
+
+<script>
+import { component } from 'node_modules/vue/types/umd'
+import { Component, Vue, Watch } from 'vue-property-decorator'
+@component({
+  name: 'Company-name'
+})
+
+export default class extends Vue ({
+  private validateClientName = (rule: any, value: string, callback: Function) => {
+    if (value.length < 1) {
+      callback(new Error('取引先会社名を入力してください。'))
+    } else if(value.length > 50){
+      callback(new Error('文字数は50文字以内で入力してください。'))
+    }else {
+      callback()
+    }
+  }
+
+  private client = {
+    name: ''
+  }
+
+  private clientRules = {
+    name: [{validator: this.validateClientName, trigger: 'blur' }]
+  }
+
+})
+
+</script>
