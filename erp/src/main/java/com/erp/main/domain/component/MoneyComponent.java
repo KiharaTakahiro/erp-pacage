89f88597ac5c9e8f4887a4d6f0681fb2ee7385a6
永戸健太郎 <kntru0218gj@gmail.com>
2021-06-03 16:19:35 +0900

    ベースレスポンス継承

--- erp/src/main/java/com/erp/main/domain/component/MoneyComponent.java
+++ erp/src/main/java/com/erp/main/domain/component/MoneyComponent.java
@@ -2,13 +2,15 @@ package com.erp.main.domain.component;
 
 import org.springframework.stereotype.Component;
 
+import com.erp.main.app.controller.system.response.BaseResponse;
+
 /**
  * 金額計算に関連するコンポーネント
  * @author takah
  *
  */
 @Component
-public class MoneyComponent {
+public class MoneyComponent extends BaseResponse {
 	
 	/**
 	 * 金額に対する消費税の計算を行う
