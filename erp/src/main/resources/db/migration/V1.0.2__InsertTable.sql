9b4d8f9f679d348b27e360acd8863a252a13e288
永戸健太郎 <kntru0218gj@gmail.com>
2021-06-04 16:17:29 +0900

    DBおっけー

--- erp/src/main/resources/db/migration/V1.0.2__InsertTable.sql
+++ erp/src/main/resources/db/migration/V1.0.2__InsertTable.sql
@@ -1,8 +1,7 @@
--- 採番テーブル
--- DROP TABLE public.sequence;
+-- システム TAXの値
 INSERT INTO public.system(
 	key, insert_date, update_date, version, value
 )
 VALUES(
-	'tax', NOW(), NOW(), 0, "10"
+	'tax', NOW(), NOW(), 0, 10
 );
