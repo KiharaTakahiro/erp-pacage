651a402cf039d44b46ba15c0d116e396c86e119f
永戸健太郎 <kntru0218gj@gmail.com>
2021-06-04 10:41:11 +0900

    Repositoryの作成

--- erp/src/main/java/com/erp/main/domain/component/MoneyComponent.java
+++ erp/src/main/java/com/erp/main/domain/component/MoneyComponent.java
@@ -12,12 +12,23 @@ import com.erp.main.app.controller.system.response.BaseResponse;
 @Component
 public class MoneyComponent extends BaseResponse {
 	
+	@Autowirde
+	private SystemRepository
+	
+	public static final String TAX = "TAX";
+	
 	/**
 	 * 金額に対する消費税の計算を行う
 	 * @return
 	 */
 	public long computeTax(long target) {
-		// TODO: マスタ等から取得するように変更するべき
+		/*
+		 * TODO: マスタ等から取得するように変更するべき
+		 * リポジトリでデータを引いてきて、数字化して1/100にして変数に
+		 * Keyは変数にして大文字にする
+		 * private static string final クラスに書く
+		 */
+		
 		return Double.doubleToLongBits(target * 0.1);
 	}
 
--- erp/src/main/java/com/erp/main/domain/objects/entity/SystemEntity.java
+++ erp/src/main/java/com/erp/main/domain/objects/entity/SystemEntity.java
@@ -2,7 +2,7 @@ package com.erp.main.domain.objects.entity;
 
 import com.erp.main.domain.objects.entity.fields.SystemEntityFields;
 
-public class SyetemEntity extends SystemEntityFields {
+public class SystemEntity extends SystemEntityFields {
 	
 
 }
