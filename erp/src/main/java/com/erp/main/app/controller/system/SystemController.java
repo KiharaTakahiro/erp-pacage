d1c9a1d0c0defd74e77593a7c1be470cac96b646
永戸健太郎 <kntru0218gj@gmail.com>
2021-06-09 15:11:16 +0900

    バグ修正

--- erp/src/main/java/com/erp/main/app/controller/system/SystemController.java
+++ erp/src/main/java/com/erp/main/app/controller/system/SystemController.java
@@ -1,7 +1,6 @@
 package com.erp.main.app.controller.system;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -107,7 +106,7 @@ public class SystemController {
 	}
 
 	
-	@GetMapping("/clients/id/edit")
+	@PostMapping("/clients/edit")
 	public ClientResponse getClient(@RequestBody GetClientRequest request) {
 		Long id = 2L; 
 		GetClientVo vo = this.masterService.getClientVo(id);
