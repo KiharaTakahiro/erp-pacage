732b840497514e2b46a95f72ffecd14d74364fce
永戸健太郎 <kntru0218gj@gmail.com>
2021-06-17 16:03:28 +0900

    e-mailのヴァリデーション

--- erp/web/src/views/components/email.vue
+++ erp/web/src/views/components/email.vue
@@ -2,7 +2,8 @@
 <el-form-item 
   label="メールアドレス"
   :rules="[
-    { required: true, message: 'メールアドレスは必ず入力してください', trigger: 'blur'}
+    { required: true, message: 'メールアドレスは必ず入力してください', trigger: 'blur'},
+    { type: 'email', message: 'メールアドレスが不正です', trigger: ['blur', 'change'] }
   ]"
   prop="email"
   >
