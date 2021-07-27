7a83f5483ab971eee78014dffaeddd59c4f1d315
永戸健太郎 <kntru0218gj@gmail.com>
2021-07-27 10:21:56 +0900

    修正

--- erp/src/main/java/com/erp/main/domain/objects/entity/SupplierProductEntity.java
+++ erp/src/main/java/com/erp/main/domain/objects/entity/SupplierProductEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
  *
  */
 @Entity
-@Table(name="SupllierProduct")
+@Table(name="SupplierProduct")
 public class SupplierProductEntity extends SupplierProductEntityFileds {
 
 }
