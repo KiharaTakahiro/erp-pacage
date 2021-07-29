427d513f382315ed834f99e3b878d1721b9db280
AzureAD\高山　晴渡 <takayama@offgrid.co.jp>
2021-07-29 17:32:59 +0900

    カバレッジ100%

--- erp/src/test/java/com/erp/main/domain/service/SupplierServiceTest.java
+++ erp/src/test/java/com/erp/main/domain/service/SupplierServiceTest.java
@@ -6,11 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
+import com.erp.main.domain.common.exception.AppException;
 import com.erp.main.domain.component.MoneyComponent;
 import com.erp.main.domain.objects.entity.OrderDetailEntity;
 import com.erp.main.domain.objects.entity.OrderEntity;
@@ -23,7 +29,11 @@ import com.erp.main.domain.repository.SupplierProductRepository;
 import com.erp.main.domain.repository.SupplierRepository;
 import com.erp.main.domain.services.SupplierService;
 
+
+@MockitoSettings(strictness = Strictness.LENIENT)
+@ExtendWith(MockitoExtension.class)
 public class SupplierServiceTest {
+	
 	@InjectMocks
 	private SupplierService supplierService = new SupplierService();
 	
@@ -61,12 +71,14 @@ public class SupplierServiceTest {
 		CreateOrderVo createOrderVo = this.createDefaultInputData();
 		Optional<SupplierEntity> supplier = this.createDefaultSupplierData();
 		Optional<SupplierProductEntity> supplierProduct = this.createDefaultSupplierProductData();		
+		
 		// 取得処理をモック化 (仕入先情報)
 		Mockito.when(this.supplierRepository.findById(2L)).thenReturn(supplier);
 		// 取得処理をモック化 (仕入商品)
 		Mockito.when(this.supplierProductRepository.findById(2L)).thenReturn(supplierProduct);
 		// 消費税はサービスのテストでは10%として考える
 		Mockito.when(this.moneyComponent.computeTax(700L, null)).thenReturn(70L);
+		
 		// 処理の実行
 		this.supplierService.createOrder(createOrderVo);
 		
@@ -84,21 +96,83 @@ public class SupplierServiceTest {
 	
 	/**
 	 * 異常系1
-	 * 取引先情報がないパターン
+	 * 仕入先情報がないパターン
 	 */
-//	@Test
-//	public void createOrderErrorCase1() {
-//		// 実行用テストデータの作成
-//		CreateOrderVo createOrderVo = this.createDefaultInputData();
-//		Optional<SupplierEntity> supplier = this.createDefaultSupplierData();
-//		
-//		// 取得処理をモック化
-//		Mockito.when(this.supplierProductRepository.findById(2L)).thenReturn(supplier);
-//		// 消費税はサービスのテストでは10%として考える
-//		Mockito.when(this.moneyComponent.computeTax(700L, null)).thenReturn(70L);
-//		// 処理の実行
-//		Assertions.assertThrows(AppException.class, () -> orderService.createSupplier(createSupplier));
-//		}
+	@Test
+	public void createOrderErrorCase1() {
+		// 実行用テストデータの作成
+		CreateOrderVo createOrderVo = this.createDefaultInputData();
+		Optional<SupplierEntity> supplier = this.createErrorSupplierData();
+		Optional<SupplierProductEntity> supplierProduct = createDefaultSupplierProductData();
+		
+		// 取得処理をモック化 (仕入先情報)
+		Mockito.when(this.supplierRepository.findById(2L)).thenReturn(supplier);
+		Mockito.when(this.supplierProductRepository.findById(2L)).thenReturn(supplierProduct);
+		// 消費税はサービスのテストでは10%として考える
+		Mockito.when(this.moneyComponent.computeTax(700L, null)).thenReturn(70L);
+		// 処理の実行
+		Assertions.assertThrows(AppException.class, () -> supplierService.createOrder(createOrderVo));
+	}
+	
+	/**
+	 * 異常系2
+	 * 仕入先商品がないパターン
+	 */
+	@Test
+	public void createOrderErrorCase2() {
+		// 実行用テストデータの作成
+		CreateOrderVo createOrderVo = this.createDefaultInputData();
+		Optional<SupplierEntity> supplier = this.createDefaultSupplierData();
+		Optional<SupplierProductEntity> supplierProduct = createErrorSupplierProductData();
+		
+		// 取得処理をモック化 (仕入先情報)
+		Mockito.when(this.supplierRepository.findById(2L)).thenReturn(supplier);
+		Mockito.when(this.supplierProductRepository.findById(2L)).thenReturn(supplierProduct);
+		// 消費税はサービスのテストでは10%として考える
+		Mockito.when(this.moneyComponent.computeTax(700L, null)).thenReturn(70L);
+		// 処理の実行
+		Assertions.assertThrows(AppException.class, () -> supplierService.createOrder(createOrderVo));
+	}
+	
+	/**
+	 * 異常系3
+	 * 商品情報がないパターン
+	 */
+	@Test
+	public void createOrderErrorCase3() {
+		// 実行用テストデータの作成
+		CreateOrderVo createOrderVo = this.createErrorInputData();
+		Optional<SupplierEntity> supplier = this.createDefaultSupplierData();
+		Optional<SupplierProductEntity> supplierProduct = createDefaultSupplierProductData();
+		
+		// 取得処理をモック化 (仕入先情報)
+		Mockito.when(this.supplierRepository.findById(2L)).thenReturn(supplier);
+		Mockito.when(this.supplierProductRepository.findById(2L)).thenReturn(supplierProduct);
+		// 消費税はサービスのテストでは10%として考える
+		Mockito.when(this.moneyComponent.computeTax(700L, null)).thenReturn(70L);
+		// 処理の実行
+		Assertions.assertThrows(AppException.class, () -> supplierService.createOrder(createOrderVo));
+	}
+	
+	/**
+	 * 異常系4
+	 * 詳細情報がないパターン
+	 */
+	@Test
+	public void createOrderErrorCase4() {
+		// 実行用テストデータの作成
+		CreateOrderVo createOrderVo = this.createErrorInputData2();
+		Optional<SupplierEntity> supplier = this.createDefaultSupplierData();
+		Optional<SupplierProductEntity> supplierProduct = createDefaultSupplierProductData();
+		
+		// 取得処理をモック化 (仕入先情報)
+		Mockito.when(this.supplierRepository.findById(2L)).thenReturn(supplier);
+		Mockito.when(this.supplierProductRepository.findById(2L)).thenReturn(supplierProduct);
+		// 消費税はサービスのテストでは10%として考える
+		Mockito.when(this.moneyComponent.computeTax(700L, null)).thenReturn(70L);
+		// 処理の実行
+		Assertions.assertThrows(AppException.class, () -> supplierService.createOrder(createOrderVo));
+	}
 	
 	/**
 	 * 仕入先データ生成
@@ -117,6 +191,7 @@ public class SupplierServiceTest {
 	private Optional<SupplierProductEntity> createDefaultSupplierProductData(){
 		SupplierProductEntity supplierProducts = new SupplierProductEntity();
 		supplierProducts.setSupplierProductSeq(2L);
+		supplierProducts.setPurchaseUnitPrice(10L);
 		return Optional.of(supplierProducts);
 	}
 	
@@ -139,7 +214,7 @@ public class SupplierServiceTest {
 		// 発注ステータス
 		createOrderDetailVo.setStatus(null);
 		// 数量
-		createOrderDetailVo.setQuantity(4);
+		createOrderDetailVo.setQuantity(2);
 		// 納期
 		createOrderDetailVo.setDeriveryDate("20210503");
 		
@@ -175,4 +250,83 @@ public class SupplierServiceTest {
 		return entity;
 	}
 
+	
+	/**
+	 * エラー用の仕入先データ生成
+	 * @return
+	 */
+	private Optional<SupplierEntity> createErrorSupplierData(){
+		// 仕入先を空に
+		return Optional.empty();
+	}
+
+	/**
+	 * エラー用の仕入商品データ生成
+	 * @return
+	 */
+	private Optional<SupplierProductEntity> createErrorSupplierProductData(){
+		// 仕入先を空に
+		return Optional.empty();
+	}
+
+	/**
+	 * 数量のエラー用の商品データ生成
+	 * @return
+	 */
+	private CreateOrderVo createErrorInputData() {
+		CreateOrderVo createOrderVo = new CreateOrderVo();
+		// 仕入先SEQ
+		createOrderVo.setSupplierSeq(2L);
+		// 発注金額
+		createOrderVo.setTotal(2L);
+		// 消費税
+		createOrderVo.setTax(2L);
+		
+		CreateOrderDetailVo createOrderDetailVo = new CreateOrderDetailVo();
+		// 仕入商品Seq
+		createOrderDetailVo.setSupplierProductSeq(2L);
+		// 発注ステータス
+		createOrderDetailVo.setStatus(null);
+		// 数量
+		createOrderDetailVo.setQuantity(0);
+		// 納期
+		createOrderDetailVo.setDeriveryDate("20210503");
+		
+		List<CreateOrderDetailVo> details = new ArrayList<>();
+		details.add(createOrderDetailVo);
+		createOrderVo.setDetails(details);
+		
+		return createOrderVo;
+	}
+	
+	
+	/**
+	 * 詳細情報のエラー用の商品データ生成
+	 * @return
+	 */
+	private CreateOrderVo createErrorInputData2() {
+		CreateOrderVo createOrderVo = new CreateOrderVo();
+		// 仕入先SEQ
+		createOrderVo.setSupplierSeq(2L);
+		// 発注金額
+		createOrderVo.setTotal(2L);
+		// 消費税
+		createOrderVo.setTax(2L);
+
+		CreateOrderDetailVo createOrderDetailVo = new CreateOrderDetailVo();
+		// 仕入商品Seq
+		createOrderDetailVo.setSupplierProductSeq(2L);
+		// 発注ステータス
+		createOrderDetailVo.setStatus(null);
+		// 数量
+		createOrderDetailVo.setQuantity(0);
+		// 納期
+		createOrderDetailVo.setDeriveryDate("20210503");
+		
+		List<CreateOrderDetailVo> details = new ArrayList<>();
+		createOrderVo.setDetails(details);
+		
+		return createOrderVo;
+	}
+	
 }
