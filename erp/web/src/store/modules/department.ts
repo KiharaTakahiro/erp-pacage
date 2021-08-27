cc109f8af424bdc6dba87a9363442c59b3ba8660
永戸健太郎 <kntru0218gj@gmail.com>
2021-08-27 17:27:32 +0900

    部署も

---
+++ erp/web/src/store/modules/department.ts
@@ -0,0 +1,30 @@
+import { VuexModule, Module, Mutation, Action, getModule } from 'vuex-module-decorators'
+import store from '@/store'
+export interface IDepartmentState {
+  id: string,
+  companyId: string,
+  name: string
+}
+
+@Module({ dynamic: true, store, name: 'client' })
+class Department extends VuexModule implements IDepartmentState {
+  public id = ""
+  public companyId = ""
+  public name = ""
+  @Mutation
+  private SET_ID(id: string){
+    this.id = id
+  }
+
+  @Mutation
+  private SET_NAME(name: string){
+    this.name = name
+  }
+
+  @Mutation
+  private SET_COMPANY_ID(companyId: string){
+    this.companyId = companyId
+  }
+
+}
+export const DepartmentModule = getModule(Department)
