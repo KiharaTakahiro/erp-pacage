3316351bd0119a4a0c7481a10341fad715729c48
AzureAD\高山　晴渡 <takayama@offgrid.co.jp>
2021-09-08 08:12:58 +0900

    倉庫新規作成

---
+++ erp/web/src/views/components/warehouse-name.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-form-item
+    :rules="[
+      {
+        required: true,
+        message: '倉庫名は必ず入力してください',
+        trigger: 'blur',
+      },
+      { max: 50, message: '文字数は50文字までにしてください' },
+    ]"
+    prop="name"
+  >
+    <el-input
+      v-model="name"
+      :placeholder="$t('warehouse.name')"
+      type="text"
+      maxlength="50"
+      tabindex="1"
+      autocomplete="on"
+    />
+  </el-form-item>
+</template>
+
+<script lang="ts">
+import { Component, Vue, PropSync } from 'vue-property-decorator'
+import '@/assets/custom-theme/index.css'
+
+@Component({
+  name: 'WarehouseName',
+})
+export default class extends Vue {
+  @PropSync('WarehouseName', { type: String }) name!: string
+}
+</script>
+
+<style lang="scss" scoped>
+.field-label {
+  vertical-align: middle;
+}
+
+.app-container {
+  width: 50%;
+}
+
+.box-card {
+  width: 400px;
+  max-width: 100%;
+  margin: 20px auto;
+}
+
+.block {
+  padding: 30px 24px;
+}
+
+.tag-item {
+  margin-right: 15px;
+}
+
+.complete-btn {
+  float: right;
+}
+</style>
