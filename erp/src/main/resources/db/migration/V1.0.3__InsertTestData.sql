8fe74152c95deadbe8df619ab252b4b8dea42642
AzureAD\高山　晴渡 <takayama@offgrid.co.jp>
2021-10-01 03:31:36 +0900

    sql 追記

--- erp/src/main/resources/db/migration/V1.0.3__InsertTestData.sql
+++ erp/src/main/resources/db/migration/V1.0.3__InsertTestData.sql
@@ -1,4 +1,5 @@
 --testデータを入れる
+--会社テストデータ
 INSERT 
 INTO public.company (company_seq, address, name, phone, zip) VALUES 
 (1, '東京都千代田区丸の内〇-〇-〇', '宮木総合商事', '03-〇〇〇-〇〇〇', '○○○○'),
@@ -7,9 +8,20 @@ INTO public.company (company_seq, address, name, phone, zip) VALUES
 (4, '東京都豊島区西池袋〇-〇-〇', '高山商店', '03-〇〇〇-〇〇〇', '○○○○')
 ;
 
+--クライアントテストデータ
 INSERT 
 INTO PUBLIC.clients(clients_seq, name) VALUES 
 (1, '佐藤太郎'), 
 (2, '鈴木一郎'), 
 (3, '山田花子'), 
-(4, '小林優太');
+(4, '小林優太')
+;
+
+--部署テストデータ
+INSERT 
+INTO PUBLIC.department(department_seq, name) 
+VALUES (1, '人事部'), 
+(2, '総務部'), 
+(3, '経理部'), 
+(4, '営業部')
+;
